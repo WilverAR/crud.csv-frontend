21eaf7df69f5fe46378a9700a3fc593bb29c4382
Wilver Arana R <u202023992@upc.edu.pe>
2023-12-17 15:59:26 -0500

    feat(resale): add service TransactionService

---
+++ src/app/resale/services/transaction.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpBaseService } from "../../shared/services/http-base.service";
+import { Transaction } from "../model/Transaction";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransactionService extends HttpBaseService<Transaction> {
+  constructor(http: HttpClient) {
+    super(http);
+    this.resourceEndpoint = '/transactions';
+  }
+}
