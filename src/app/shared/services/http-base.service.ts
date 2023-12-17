a39e513bd9f77b709c0c5fd3ac5a6ade8ffffe66
Wilver Arana R <u202023992@upc.edu.pe>
2023-12-17 15:58:22 -0500

    feat(shared): add service HttpBaseService

---
+++ src/app/shared/services/http-base.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
+import { catchError, Observable, retry, throwError } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpBaseService<T> {
+  basePath: string = environment.serverBasePath;
+  resourceEndpoint: string = '/resources';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+  constructor(private http: HttpClient) { }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.log('An error occurred: ', error.error.message);
+    }
+    else {
+      console.log(`Backend returned code ${error.status}, body was: ${error.error}`);
+    }
+    return throwError('Something bad happened; please try again later.');
+  }
+  private resourcePath() {
+    return this.basePath + this.resourceEndpoint;
+  }
+
+  getAll(): Observable<T> {
+    console.log('Getting all resources from server.');
+    return this.http.get<T>(this.resourcePath(), this.httpOptions).pipe(retry(2), catchError(this.handleError));
+  }
+
+  getOne(id: number): Observable<T> {
+    return this.http.get<T>(this.resourcePath() + '/' + id, this.httpOptions).pipe(retry(2), catchError(this.handleError));
+  }
+
+  create(item: T): Observable<T> {
+    return this.http.post<T>(this.resourcePath(), JSON.stringify(item), this.httpOptions).pipe(retry(2), catchError(this.handleError));
+  }
+
+  update(id: number, item: T): Observable<T> {
+    return this.http.put<T>(this.resourcePath() + '/' + id, JSON.stringify(item), this.httpOptions).pipe(retry(2), catchError(this.handleError));
+  }
+
+  delete(id: number) {
+    return this.http.delete<T>(this.resourcePath() + '/' + id, this.httpOptions).pipe(retry(2), catchError(this.handleError));
+  }
+}
