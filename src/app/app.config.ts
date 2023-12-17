b2d944e06d72215f3e1e3fde96448d23d7c42170
Wilver Arana R <u202023992@upc.edu.pe>
2023-12-17 15:54:15 -0500

    fix(app): add ApplicationConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {HttpClientModule} from "@angular/common/http";
+import {HttpBaseService} from "./shared/services/http-base.service";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), HttpBaseService, importProvidersFrom(HttpClientModule)]
 };
