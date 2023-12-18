441d8558ba001545b01184bc1002b4fbba03c488
Wilver Arana R <u202023992@upc.edu.pe>
2023-12-17 19:00:15 -0500

    feat(resale): update page HomeComponent

--- src/app/resale/pages/home/home.component.ts
+++ src/app/resale/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {TableComponent} from "../../components/table/table.component";
 import {TableModule} from "primeng/table";
 import {ToolbarComponent} from "../../components/toolbar/toolbar.component";
+import {DialogComponent} from "../../components/dialog/dialog.component";
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,8 @@ import {ToolbarComponent} from "../../components/toolbar/toolbar.component";
   imports: [
     TableComponent,
     TableModule,
-    ToolbarComponent
+    ToolbarComponent,
+    DialogComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
