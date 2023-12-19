2a4f20ceb798867a796c5d755dc9b38e4eeb2768
Wilver Arana R <u202023992@upc.edu.pe>
2023-12-18 20:36:16 -0500

    feat(resale): update component Table

--- src/app/resale/components/table/table.component.ts
+++ src/app/resale/components/table/table.component.ts
@@ -1,22 +1,15 @@
-import {Component, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {TableModule} from "primeng/table";
 import {NgForOf, NgIf} from "@angular/common";
 import {PaginatorModule, PaginatorState} from "primeng/paginator";
 import {ToastModule} from "primeng/toast";
 import {ButtonModule} from "primeng/button";
 import {RippleModule} from "primeng/ripple";
-import {FileUploadModule} from "primeng/fileupload";
-import {ToolbarModule} from "primeng/toolbar";
-import {DialogModule} from "primeng/dialog";
-import {InputTextModule} from "primeng/inputtext";
-import {SpeedDialModule} from "primeng/speeddial";
-import {CalendarModule} from "primeng/calendar";
-import {KeyFilterModule} from "primeng/keyfilter";
-import {InputTextareaModule} from "primeng/inputtextarea";
 import {Transaction} from "../../model/transaction";
 import {TransactionService} from "../../services/transaction.service";
 import {ConfirmationService, MessageService} from "primeng/api";
 import {DialogComponent} from "../dialog/dialog.component";
+import {ConfirmDialogModule} from "primeng/confirmdialog";
 
 
 interface Column {
@@ -38,66 +31,65 @@ interface ExportColumn {
     ToastModule,
     ButtonModule,
     RippleModule,
-    FileUploadModule,
-    ToolbarModule,
-    DialogModule,
-    InputTextModule,
     NgIf,
-    SpeedDialModule,
-    CalendarModule,
-    KeyFilterModule,
-    InputTextareaModule,
-    DialogComponent
+    DialogComponent,
+    ConfirmDialogModule
   ],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
-export class TableComponent implements OnChanges {
+export class TableComponent {
   @Input() transactions: Transaction[] = [];
   @Input() columns: Column[] = [];
   @Input() exportColumns!: ExportColumn[];
-  first: number = 0;
-  rows: number = 5;
+  @Input() loading: boolean = true;
 
-  @Input() totalRecords: number = this.transactions.length;
   transactionDialog: boolean = false;
   submitted: boolean = false;
+  first: number = 0;
+  rows: number = 5;
+  totalRecords: number = this.transactions.length;
   transaction!: Transaction;
-  @Input() loading: boolean = true;
+
+  constructor(private transactionService: TransactionService, private messageService: MessageService, private confirmationService: ConfirmationService) { }
 
   onPageChange(event: PaginatorState) {
     this.first = event.first || 0;
   }
-  constructor(private transactionService: TransactionService, private messageService: MessageService) { }
-
-  ngOnChanges(changes:SimpleChanges) {
-    console.log('Changes: ', changes);
-    this.totalRecords = this.transactions.length;
-  }
   editProduct(transaction: any) {
     this.transaction = { ...transaction };
-    console.log('Transaction: ', transaction);
     this.transactionDialog = true;
   }
-  deleteProduct(transaction: any) {
-    this.transaction = { ...transaction };
-    this.transactionService.delete(this.transaction.id).subscribe((response: any) => {
-      console.log('Response: ', response);
-    });
-    this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Products Deleted', life: 3000 });
-  }
+  deleteProduct(transactionId: number, event: Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Do you want to delete this record?',
+      header: 'Delete Confirmation',
+      icon: 'pi pi-info-circle',
+      acceptButtonStyleClass:"p-button-danger p-button-text",
+      rejectButtonStyleClass:"p-button-text p-button-text",
+      acceptIcon:"none",
+      rejectIcon:"none",
 
+      accept: () => {
+        this.transactionService.delete(transactionId).subscribe(() => {
+          this.transactionService.getAll().subscribe();
+          this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Transaction Deleted', life: 3000 });
+        });
+      },
+      reject: () => {
+        this.transactionDialog = false;
+      }
+    });
+  }
   onDisplayDialog($event: boolean) {
     this.transactionDialog = $event;
   }
-
   onSubmittedDialog($event: boolean) {
     this.submitted = $event;
   }
-
   onDisplay() {
     this.transaction = <Transaction>{};
     this.transactionDialog = true;
-    console.log('Transaction Dialog: ', this.transactionDialog);
   }
 }
