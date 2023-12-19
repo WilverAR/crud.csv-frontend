7a66506c3d4f8a3bcc88fe3a2cd09ac9b3c6c1b6
Wilver Arana R <u202023992@upc.edu.pe>
2023-12-18 22:45:46 -0500

    fix(resale): update component Toolbar and Table

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,13 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import {MenubarModule} from "primeng/menubar";
 import {InputTextModule} from "primeng/inputtext";
+import {AvatarModule} from "primeng/avatar";
+import {ToolbarModule} from "primeng/toolbar";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     MenubarModule,
-    InputTextModule
+    InputTextModule,
+    AvatarModule,
+    ToolbarModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
